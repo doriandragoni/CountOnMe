f4c8e757f8e1dac4ee400b7d295f57ff1cd03312
Dorian Dragoni <doriandragoni@icloud.com>
2022-09-01 22:29:36 +0200

    Reorganization of files

---
+++ CountOnMe/Model/CalculationManager.swift
@@ -0,0 +1,9 @@
+//
+//  CalculationManager.swift
+//  CountOnMe
+//
+//  Created by Dorian Dragoni on 01/09/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
