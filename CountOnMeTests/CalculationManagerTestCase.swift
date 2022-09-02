47232cdad40b17f479cc5d0e662e12ce8efd15d6
Dorian Dragoni <doriandragoni@icloud.com>
2022-09-02 15:17:22 +0200

    Rename unit test file

--- CountOnMeTests/CalculationManagerTestCase.swift
+++ CountOnMeTests/CalculationManagerTestCase.swift
@@ -1,5 +1,5 @@
 //
-//  CountOnMeTests.swift
+//  CalculationManagerTestCase.swift
 //  CountOnMeTests
 //
 //  Created by Dorian Dragoni on 02/09/2022.
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class CountOnMeTests: XCTestCase {
+class CalculationManagerTestCase: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
